58bd89a1b15ad582a99bf1999709cdeedf4e6185
Berk Altun <contact@vberkaltun.com>
2017-06-15 10:58:19 +0300

    Popup alt yapısı oluşturuldu.

--- web/_outlines/page_main.js
+++ web/_outlines/page_main.js
@@ -20,3 +20,31 @@ $(document).ready(function () {
 $(window).resize(function () {
     $('.table_news').css('height', $("body").height() - $('.table_weather').innerHeight() - em(5.000));
 });
+
+/* WHEN POPUP TRIGGERED */
+
+// Get the popup
+var popup = document.getElementById('popup');
+
+// Get the trigger that opens the popup
+var trigger = document.getElementById("trigger");
+
+// Get the <close> element that closes the popup
+var close = document.getElementsByClassName("close")[0];
+
+// When the user clicks the trigger, open the popup 
+trigger.onclick = function () {
+    popup.style.display = "block";
+}
+
+// When the user clicks on <close> (x), close the popup
+close.onclick = function () {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function (event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
