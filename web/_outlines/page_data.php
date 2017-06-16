d65955df440b33a5923d28a917c7685830780d6b
Berk Altun <contact@vberkaltun.com>
2017-06-17 01:27:57 +0300

    Duyuru ekleyen kod betiği üzerinde major güncelleme...

--- web/_outlines/page_data.php
+++ web/_outlines/page_data.php
@@ -3,11 +3,15 @@
 	if($conn -> connect_error){
 		die("Connection error: ". $conn -> connect_error);
 	}
+
 	$result = $conn -> query("SELECT * FROM notices WHERE endDate >= CURDATE()");
 	if($result -> num_rows > 0){
 		while($row = $result -> fetch_assoc()){
-			echo $row['date'].'<br>';
-			echo $row['content'].'<br><br>';
+            echo "<tr><td>";
+			echo $row['content'];
+            echo "</td><td><span class='status_outside status_other'><span class='status_inside'>";
+            echo $row['endDate'];
+            echo "</span></span></td></tr>";
 		}
 	}
 ?>
